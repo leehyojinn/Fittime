f91b3dfa4342dfc0686f5ebd3b3f44599f4e00c0
hyojin <zzzzaok@gmail.com>
2025-05-20 14:53:02 +0900

    프로필 이미지 dto

---
+++ Back/src/main/java/com/fitime/dto/Profile_fileDTO.java
@@ -0,0 +1,29 @@
+package com.fitime.dto;
+
+public class Profile_fileDTO {
+	
+	private String user_id;
+	private String file_name;
+	private String category;
+
+	   
+   public String getUser_id() {
+      return user_id;
+   }
+   public void setUser_id(String user_id) {
+      this.user_id = user_id;
+   }
+   public String getFile_name() {
+      return file_name;
+   }
+   public void setFile_name(String file_name) {
+      this.file_name = file_name;
+   }
+   public String getCategory() {
+      return category;
+   }
+   public void setCategory(String category) {
+      this.category = category;
+   }
+
+}
