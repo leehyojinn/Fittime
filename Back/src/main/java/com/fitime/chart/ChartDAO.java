3d48fc17cf88b37736663752967b55fd65457459
KimDaeui <KimDaeui@github.com>
2025-05-22 11:51:14 +0900

    해당 센터 통계 내용

---
+++ Back/src/main/java/com/fitime/chart/ChartDAO.java
@@ -0,0 +1,34 @@
+package com.fitime.chart;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ChartDAO {
+	
+	List<Map<String, Object>> memberChart(String center_id);
+
+	List<Map<String, Object>> productChart(String center_id);
+	
+	List<Map<String, Object>> currentTrainerChart(int center_idx);
+	
+	List<Map<String, Object>> currentSalesChart(String center_id);
+	
+	List<Map<String, Object>> salesChart(String center_id);
+
+	List<Map<String, Object>> bookChart(String center_id);
+
+	List<Map<String, Object>> productSalesChart(String center_id);
+
+	List<Map<String, Object>> trainerBookChart(String center_id);
+
+	List<Map<String, Object>> trainerRatingChart(int center_idx);
+
+	List<Map<String, Object>> productPopularChart(String center_id);
+
+	List<Map<String, Object>> trainerChart(int center_idx);
+
+
+}
