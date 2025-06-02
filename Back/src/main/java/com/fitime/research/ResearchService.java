bb12d9186c29b1eabdd8cc48e4dbfe2f81646b1a
LeeJunHyuk <wnsgur0813@gmail.com>
2025-06-02 16:33:24 +0900

    검색 수정

--- Back/src/main/java/com/fitime/research/ResearchService.java
+++ Back/src/main/java/com/fitime/research/ResearchService.java
@@ -25,12 +25,13 @@ public class ResearchService {
 
 	public Map<String, Object> searchName(Map<String, Object> param) {
 		Map<String, Object>map = new HashMap<String, Object>();
-		int page = ((int)param.get("page")-1)*5;
+		int page = ((Integer)param.get("page")-1)*5;
 		String name = (String)param.get("name");
 		List<Map<String, Object>> result = dao.trainerName(name,page);
 		map.put("trainer_list", result);
 		result = dao.centerName(name,page);
 		map.put("center_list", result);
+		logger.info("map : {}",map);
 		return map;
 	}
 
